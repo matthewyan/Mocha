e50f9ac67a4946a366b45019e64489c833d10444
matthewyan <matthewyan@tencent.com>
2019-12-19 21:16:44 +0800

    补充些信息 & 修改问题

--- demo03/test/add.test.js
+++ demo03/test/add.test.js
@@ -2,6 +2,8 @@ var add = require('../src/add.js');
 var expect = require('chai').expect;
 
 describe('add-加法函数的测试', function() {
+    // only是表示只执行当前一个测试用例 (如果有多个only用例，则only的都会执行)
+    // multiply.test.js中的用例也会受影响
     it.only('1 加 1 应该等于 2', function() {
         expect(add(1, 1)).to.be.equal(2);
     });
